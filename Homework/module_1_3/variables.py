b7c822e1093b004854e2ecf6b5a9b21f7a276e7b
Xenon998 <kseniazevak@gmail.com>
2024-10-12 23:27:40 +0300

    module_1_3

---
+++ Homework/module_1_3/variables.py
@@ -0,0 +1,5 @@
+homework = '12'
+hours = '1.5'
+course = 'Python'
+average_time = float(hours) / int(homework)
+print(course, ', всего задач:', homework, ', затрачено часов:', hours, ', среднее время выполнения:', average_time)
