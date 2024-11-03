db310ab6891ecc87fd4f1033e509359dc4ab493e
Xenon998 <kseniazevak@gmail.com>
2024-11-03 20:05:31 +0300

    module_1_5

--- Homework/module_1_5/Immutable and mutable objects. Tuples.py
+++ Homework/module_1_5/Immutable and mutable objects. Tuples.py
@@ -1,17 +1,16 @@
 # Part 1
-immutable_var = 1, 2, 3, 4, True
+immutable_var = (123, True, "string")
 print(immutable_var)
 
 # Part 2
-immutable_var[0][0] = 5
+immutable_var[0] = 5
 print(immutable_var)
 # Суть кортежа состоит в том, что он неизменяем и чаще всего
 # используется для тех списков, что мы не хотим затронуть.
 # Но несмотря на его неизменность, мы также можем хранить в кортеже и изменяемые объекты.
 
 # Part 3
-mutable_list = ([11, 22, 33], 'mouse', 2)
+mutable_list = ["A", "B", "C"]
 print(mutable_list)
-mutable_list[0][0]= 'mode'
-mutable_list[0][1]= 'apple'
-print(mutable_list)
+mutable_list[0] = "new"
+print("Измененный список:", mutable_list)
