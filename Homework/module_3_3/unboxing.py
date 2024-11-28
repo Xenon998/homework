49855bbcd1c7cff5ffa4d5c45850cee97e1b4135
Xenon998 <kseniazevak@gmail.com>
2024-11-28 23:32:54 +0300

    module_3_3

--- Homework/module_3_3/unboxing.py
+++ Homework/module_3_3/unboxing.py
@@ -4,7 +4,7 @@ def print_params(a = 1, b = 'stroka', c = True):
     print("b:", b)
     print("c:", c)
 
-print_params()
+#print_params()
 print_params(b = 'new_stroka')
 print_params(2, "another_stroka", False)
 
@@ -20,11 +20,9 @@ values_dict = {
     "b": "text",
     "c": False
 }
-try:
-    print_params(*values_list, **values_dict)
-except Exception as e:
-    print(e)
+print_params(*values_dict)
+print_params(**values_dict)
 
 #3.Распаковка + отдельные параметры:
-values_list_2 = [100, 'blablabla' ]
+values_list_2 = [100, 'blablabla']
 print_params(*values_list_2, 52)
